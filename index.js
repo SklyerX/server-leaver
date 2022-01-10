f06bb91a9860302f3a860d155df1c8d709f9d298
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2022-01-10 11:46:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,8 @@ client.on("ready", async() => {
  
     const readyOrNot = await input.text("Are you ready to start (y / n): ")
     if(readyOrNot === "y" || "Y") {
-        if (client.guild.id === settings.whitelistedServers) {
-            return
-        } else {
             client.guilds.cache.forEach(guild => guild.leave());
         }
-    } else {
-        console.log("Stopped Tool")
-    }
 })
 
 client.login(settings.token)
